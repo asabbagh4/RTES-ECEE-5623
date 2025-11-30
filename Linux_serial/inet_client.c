4adaea4102741d0ae6874841ddb710d37a0dcdb0
Abdul Alsabbagh <atsabbagh@gmail.com>
2025-11-30 18:35:26 -0500

    fix warnings in client

--- Linux_serial/inet_client.c
+++ Linux_serial/inet_client.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <string.h>
+#include <unistd.h>
 
 
 #define NSTRS 3
@@ -23,9 +24,7 @@ char *strs[NSTRS] = {
 extern int errno;
 extern void broken_pipe_handler();
 
-main(argc, argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
   char c;
   FILE *fp;
